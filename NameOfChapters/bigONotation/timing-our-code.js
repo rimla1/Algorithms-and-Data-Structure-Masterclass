8cad16065718710b57d45d9f3d14f0cc2416ccd9
Almir Muminovic <projectalmir@gmail.com>
2022-07-07 00:56:18 +0200

    Using timing functions to compare time of execution for two functions

--- NameOfChapters/bigONotation/timing-our-code.js
+++ NameOfChapters/bigONotation/timing-our-code.js
@@ -1,7 +1,7 @@
 function addUpToN(n) {
   let total = 0;
   for (let i = 1; i <= n; i++) {
-    total += 1;
+    total += i;
   }
   return total;
 }
@@ -10,5 +10,12 @@ function addUpToM(m) {
   return (m * (m + 1)) / 2;
 }
 
-console.log(addUpToN(5));
-console.log(addUpToM(8));
+let t1 = performance.now();
+addUpToN(1000000);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+
+let t3 = performance.now();
+addUpToM(10000000);
+let t4 = performance.now();
+console.log(`Time Elapsed: ${(t4 - t3) / 1000} seconds.`);
