968342673e6014f7da857a1d0c8f1f9462e64822
Almir Muminovic <projectalmir@gmail.com>
2022-07-13 01:55:55 +0200

    Finding a bigO for a function that runtime scales proporcionally as n scales

--- NameOfChapters/bigONotation/bigO.js
+++ NameOfChapters/bigONotation/bigO.js
@@ -3,3 +3,13 @@
 function addUpToM(m) {
   return (m * (m + 1)) / 2;
 }
+
+// Have from 2n to 5n + 2 operations depends on input [It grows as n grows, runtime grows proparcionally]
+// Number of operations is (eventually) bounded by a multiple of n (2n, 10n, 50n)
+function addUpToN(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
