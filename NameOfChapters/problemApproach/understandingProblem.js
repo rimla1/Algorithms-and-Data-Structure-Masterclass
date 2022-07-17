e953bbdcffeb5fec24b8fa6a779664dc773eb8d3
Almir Muminovic <projectalmir@gmail.com>
2022-07-17 15:19:59 +0200

    Understanding the Problem

--- NameOfChapters/problemApproach/understandingProblem.js
+++ NameOfChapters/problemApproach/understandingProblem.js
@@ -3,4 +3,11 @@
 // we have two inputs(numbers) that goes into function
 // we should return a sum of the two numbers
 // I can determe a output based on input informations
-// I should call inputs: number1 and number2, and the output should be called sumOfTwoNumbers
+// I should call inputs: number1 and number2, and the output should be called sum, function should be named sumOfTwoNumbers
+
+const sumOfTwoNumbers = (number1, number2) => {
+  return (sum = number1 + number2);
+};
+
+const result = sumOfTwoNumbers(5, 7);
+console.log(result);
