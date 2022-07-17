95417dab37b4fa5cd7286ab7578f26d40f07c10a
Almir Muminovic <projectalmir@gmail.com>
2022-07-17 19:57:45 +0200

    simplify the problem

--- NameOfChapters/problemApproach/understandingProblem.js
+++ NameOfChapters/problemApproach/understandingProblem.js
@@ -11,3 +11,15 @@ const sumOfTwoNumbers = (number1, number2) => {
 
 const result = sumOfTwoNumbers(5, 7);
 console.log(result);
+
+// Simplified the problem
+
+const charCount = (string) => {
+  // Make object to return at end
+  // Do something
+  // Loop over string, for each character...
+  // If the char is a number/letter AND is a key in object, add one to count
+  // If the char is a number/letter AND not in object, add it to object and set value to 1
+  // If character is something else (space, period, etc.) don't do anything
+  // return object at the end
+};
