5f41f4e3a545843282312b2c7b4025b597e71df0
Almir Muminovic <projectalmir@gmail.com>
2022-07-17 15:17:39 +0200

    Understanding the problem before writeing a function

---
+++ NameOfChapters/problemApproach/understandingProblem.js
@@ -0,0 +1,6 @@
+// [Understand the Problem]
+// Restate the question!
+// we have two inputs(numbers) that goes into function
+// we should return a sum of the two numbers
+// I can determe a output based on input informations
+// I should call inputs: number1 and number2, and the output should be called sumOfTwoNumbers
