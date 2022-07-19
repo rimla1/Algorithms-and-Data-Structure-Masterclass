f3f6265e1ad840479c54e12f1a28e6805ad6470d
Almir Muminovic <projectalmir@gmail.com>
2022-07-19 13:48:08 +0200

    Refactor code

--- NameOfChapters/problemApproach/understandingProblem.js
+++ NameOfChapters/problemApproach/understandingProblem.js
@@ -5,21 +5,35 @@
 // I can determe a output based on input informations
 // I should call inputs: number1 and number2, and the output should be called sum, function should be named sumOfTwoNumbers
 
-const sumOfTwoNumbers = (number1, number2) => {
-  return (sum = number1 + number2);
-};
+// const sumOfTwoNumbers = (number1, number2) => {
+//   return (sum = number1 + number2);
+// };
 
-const result = sumOfTwoNumbers(5, 7);
-console.log(result);
+// const result = sumOfTwoNumbers(5, 7);
+// console.log(result);
 
 // Simplified the problem
 
 const charCount = (string) => {
   // Make object to return at end
-  // Do something
+  const countResult = {};
+
   // Loop over string, for each character...
-  // If the char is a number/letter AND is a key in object, add one to count
-  // If the char is a number/letter AND not in object, add it to object and set value to 1
+  for (var i = 0; i < string.length; i++) {
+    var char = string[i].toLowerCase();
+    // If the char is a number/letter AND is a key in object, add one to count
+    if (countResult[char] > 0) {
+      countResult[char]++;
+    }
+    // If the char is a number/letter AND not in object, add it to object and set value to 1
+    else {
+      countResult[char] = 1;
+    }
+  }
+  return countResult;
   // If character is something else (space, period, etc.) don't do anything
   // return object at the end
 };
+
+const consoleResult = charCount("Hello and Hi!");
+console.log(consoleResult);
