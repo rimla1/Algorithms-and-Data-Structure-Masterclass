fcdc94cec7279d9d48f6cf3a19ea5dfe7731a23e
Almir Muminovic <projectalmir@gmail.com>
2022-11-15 00:03:24 +0100

    Refactored solution with time of O(n)

--- NameOfChapters/problemSolving/multiplePointers.js
+++ NameOfChapters/problemSolving/multiplePointers.js
@@ -1,15 +1,34 @@
 // Naive solution Time: O(n^2) Space: O(1)
+// function sumZero(arr) {
+//   const pairs = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       //   console.log(arr[i], arr[j]);
+//       if (arr[i] + arr[j] === 0) {
+//         pairs.push([arr[i], arr[j]]);
+//       }
+//     }
+//   }
+//   return pairs;
+// }
+
 function sumZero(arr) {
-  const pairs = [];
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      //   console.log(arr[i], arr[j]);
-      if (arr[i] + arr[j] === 0) {
-        pairs.push([arr[i], arr[j]]);
-      }
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
     }
   }
-  return pairs;
 }
 
+// let t1 = performance.now();
 console.log(sumZero([-22, -4, -2, -1, 0, 1, 3, 4, 100]));
+// let t2 = performance.now();
+
+// console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds. `);
