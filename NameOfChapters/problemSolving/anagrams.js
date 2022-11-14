14e82630e29c937cd73072975945baf268852847
Almir Muminovic <projectalmir@gmail.com>
2022-11-14 18:26:42 +0100

    solution by professor O(n)

--- NameOfChapters/problemSolving/anagrams.js
+++ NameOfChapters/problemSolving/anagrams.js
@@ -1,29 +1,56 @@
 // Create a function that checks anagram of one string to another
-const isAnagram = (string1, string2) => {
-  if (string1.length !== string2.length) {
+// const isAnagram = (string1, string2) => {
+//   if (string1.length !== string2.length) {
+//     return false;
+//   }
+
+//   const frequencyCounterOfString1 = {};
+//   const frequencyCounterOfString2 = {};
+//   for (letter of str1) {
+//     frequencyCounterOfString1[letter] =
+//       (frequencyCounterOfString1[letter] || 0) + 1;
+//   }
+//   for (letter of str2) {
+//     frequencyCounterOfString2[letter] =
+//       (frequencyCounterOfString2[letter] || 0) + 1;
+//   }
+//   console.log(frequencyCounterOfString1);
+//   console.log(frequencyCounterOfString2);
+
+//   for (key in frequencyCounterOfString1) {
+//     console.log(key);
+//     if (frequencyCounterOfString2[key] !== frequencyCounterOfString1[key]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// console.log(isAnagram("rrimla", "Almirr"));
+
+const validAnagram = (first, second) => {
+  if (first.length !== second.length) {
     return false;
   }
 
-  const frequencyCounterOfString1 = {};
-  const frequencyCounterOfString2 = {};
-  for (letter of str1) {
-    frequencyCounterOfString1[letter] =
-      (frequencyCounterOfString1[letter] || 0) + 1;
-  }
-  for (letter of str2) {
-    frequencyCounterOfString2[letter] =
-      (frequencyCounterOfString2[letter] || 0) + 1;
-  }
-  console.log(frequencyCounterOfString1);
-  console.log(frequencyCounterOfString2);
+  const lookup = {};
 
-  for (key in frequencyCounterOfString1) {
-    console.log(key);
-    if (frequencyCounterOfString2[key] !== frequencyCounterOfString1[key]) {
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    console.log(lookup[letter]);
+    if (!lookup[letter]) {
       return false;
+    } else {
+      lookup[letter] -= 1;
     }
   }
+
   return true;
 };
 
-console.log(isAnagram("rrimla", "Almirr"));
+validAnagram("rrimla", "almirr");
