45c73d3e0805a93b7507cd79afb2d5d19fef170a
Almir Muminovic <projectalmir@gmail.com>
2022-11-19 16:52:27 +0100

    Divide and Conquer approach for solving algorithm using O(n) time complexity

---
+++ NameOfChapters/problemSolving/divideConquer.js
@@ -0,0 +1,10 @@
+function search(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(search([1, 4, 16, 25, 14, 2], 2));
