82a244ca1d92961eb97e984817ca1d137f3d7f9d
Almir Muminovic <projectalmir@gmail.com>
2022-11-19 12:15:51 +0100

    Solving sliding window approach using O(n) time complexity

--- NameOfChapters/problemSolving/slidingWindow.js
+++ NameOfChapters/problemSolving/slidingWindow.js
@@ -1,17 +1,37 @@
-const maxSubarraySum = (arr) => {
-  // make a variable that stores max sub of two digits
-  let maxSum = [];
-  for (i = 0; i < arr.length; i++) {
-    for (j = 1; j < arr.length; j++) {
-      if (maxSum.length === 0) {
-        maxSum.push(arr[i] + arr[j]);
-      }
-      if (maxSum[0] < arr[i] + arr[j]) {
-        maxSum[0] = arr[i] + arr[j];
-      }
-    }
+// function maxSubarraySum(arr, num) {
+//   if (num > arr.length) {
+//     return null;
+//   }
+//   var max = -Infinity;
+//   for (let i = 0; i < arr.length - num + 1; i++) {
+//     temp = 0;
+//     for (let j = 0; j < num; j++) {
+//       temp += arr[i + j];
+//       console.log(temp);
+//     }
+//     if (temp > max) {
+//       max = temp;
+//     }
+//   }
+//   return max;
+// }
+
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
   }
   return maxSum;
-};
+}
 
-console.log(maxSubarraySum([6, 7, 8, 2, 1, 2, 3, 4, 5, 6, 9]));
+console.log(maxSubarraySum([1, 9, 3, 6, 8, 2], 3));
+
+// [1, 9, 3] = 12
+// [9, 3, 6] = 17
