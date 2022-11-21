7fe350650aedd4618f565f38ec7742879df21a0b
Almir Muminovic <projectalmir@gmail.com>
2022-11-21 12:57:47 +0100

    solution for sameFrequency counter with O(n) time complexity

--- NameOfChapters/problemSolving/challenges.js
+++ NameOfChapters/problemSolving/challenges.js
@@ -1,31 +1,54 @@
 // Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits.
-const sameFrequency = (positiveNumber1, positiveNumber2) => {
-  const strPositiveNumber1 = positiveNumber1.toString();
-  const strPositiveNumber2 = positiveNumber2.toString();
-  if (strPositiveNumber1.length !== strPositiveNumber2.length) {
-    return false;
-  }
-  let frequencyCounter1 = {};
-  let frequencyCounter2 = {};
-  for (let number of strPositiveNumber1) {
-    frequencyCounter1[number] = (frequencyCounter1[number] || 0) + 1;
-  }
-  for (let number of strPositiveNumber2) {
-    frequencyCounter2[number] = (frequencyCounter2[number] || 0) + 1;
+// const sameFrequency = (positiveNumber1, positiveNumber2) => {
+//   const strPositiveNumber1 = positiveNumber1.toString();
+//   const strPositiveNumber2 = positiveNumber2.toString();
+//   if (strPositiveNumber1.length !== strPositiveNumber2.length) {
+//     return false;
+//   }
+//   let frequencyCounter1 = {};
+//   let frequencyCounter2 = {};
+//   for (let number of strPositiveNumber1) {
+//     frequencyCounter1[number] = (frequencyCounter1[number] || 0) + 1;
+//   }
+//   for (let number of strPositiveNumber2) {
+//     frequencyCounter2[number] = (frequencyCounter2[number] || 0) + 1;
+//   }
+
+//   for (let num of strPositiveNumber1) {
+//     if (!(num in frequencyCounter2)) {
+//       return false;
+//     }
+//     if (frequencyCounter1[num] !== frequencyCounter2[num]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+function sameFrequency(num1, num2) {
+  let strNum1 = num1.toString();
+  let strNum2 = num2.toString();
+  if (strNum1.length !== strNum2.length) return false;
+
+  let countNum1 = {};
+  let countNum2 = {};
+
+  for (let i = 0; i < strNum1.length; i++) {
+    countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1;
   }
 
-  for (let num of strPositiveNumber1) {
-    if (!(num in frequencyCounter2)) {
-      return false;
-    }
-    if (frequencyCounter1[num] !== frequencyCounter2[num]) {
-      return false;
-    }
+  for (let j = 0; j < strNum1.length; j++) {
+    countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1;
   }
+
+  for (let key in countNum1) {
+    if (countNum1[key] !== countNum2[key]) return false;
+  }
+
   return true;
-};
+}
 
-// console.log(sameFrequency(44472, 24447));
+console.log(sameFrequency(122345, 112345));
 
 // Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.
 const areThereDuplicates = (...args) => {
@@ -40,4 +63,4 @@ const areThereDuplicates = (...args) => {
   return true;
 };
 
-console.log(areThereDuplicates(7, 1, 5, 22, 3, 4, 66));
+// console.log(areThereDuplicates(7, 1, 5, 22, 3, 4, 66));
