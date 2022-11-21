58a4169fa430981fe579c494f3bd16a210f7bbae
Almir Muminovic <projectalmir@gmail.com>
2022-11-21 13:31:31 +0100

    Solution with O(logn) time complexity

--- NameOfChapters/problemSolving/challenges.js
+++ NameOfChapters/problemSolving/challenges.js
@@ -76,19 +76,23 @@
 //   return false;
 // }
 
-function areThereDuplicates(...args) {
-  // Two pointers
-  args.sort((a, b) => a > b);
-  let start = 0;
-  let next = 1;
-  while (next < args.length) {
-    if (args[start] === args[next]) {
-      return true;
-    }
-    start++;
-    next++;
-  }
-  return false;
+// function areThereDuplicates(...args) {
+//   // Two pointers
+//   args.sort((a, b) => a > b);
+//   let start = 0;
+//   let next = 1;
+//   while (next < args.length) {
+//     if (args[start] === args[next]) {
+//       return true;
+//     }
+//     start++;
+//     next++;
+//   }
+//   return false;
+// }
+
+function areThereDuplicates() {
+  return new Set(arguments).size !== arguments.length;
 }
 
 console.log(areThereDuplicates(7, 66, 1, 5, 22, 3, "2", 4, 66));
