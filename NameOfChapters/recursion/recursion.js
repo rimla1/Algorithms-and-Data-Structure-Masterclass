b7a3fe30441bbe9b381df20b90eb9f88569f0071
Almir Muminovic <projectalmir@gmail.com>
2022-11-25 20:38:58 +0100

    Creating factorial function using recursive approach

--- NameOfChapters/recursion/recursion.js
+++ NameOfChapters/recursion/recursion.js
@@ -23,7 +23,7 @@ function sumRange(num) {
   return num + sumRange(num - 1);
 }
 
-console.log(sumRange(4));
+// console.log(sumRange(4));
 
 // const factorial = (n) => {
 //   let result = 1;
@@ -33,12 +33,17 @@ console.log(sumRange(4));
 //   return result;
 // };
 
+// const factorial = (n) => {
+//   let result = 1;
+//   for (let i = n; i > 0; i--) {
+//     result *= i;
+//   }
+//   return result;
+// };
+
 const factorial = (n) => {
-  let result = 1;
-  for (let i = n; i > 0; i--) {
-    result *= i;
-  }
-  return result;
+  if (n === 1) return 1;
+  return n * factorial(n - 1);
 };
 
-console.log(factorial(4));
+console.log(factorial(6));
