8db7a9f7f5b6470aa806595029cdce34bc193a38
Almir Muminovic <projectalmir@gmail.com>
2022-11-25 23:59:43 +0100

    Solvgin algorithm with iteration or recurisive approach

--- NameOfChapters/recursion/easyChallenges.js
+++ NameOfChapters/recursion/easyChallenges.js
@@ -1,21 +1,39 @@
-const power = (num1, num2) => {
-  // Used one instead of zero because any number multiple with one is the number itself
-  let powerOfTheBaseToTheExponent = 1;
-  const recursiveFunc = (base, exponent) => {
-    // Base Case (When to stop)
-    if (exponent === 0) {
-      return;
-    }
-    // Smaller input (To have impact)
-    powerOfTheBaseToTheExponent *= base;
-    exponent--;
-    recursiveFunc(base, exponent);
-  };
-  recursiveFunc(num1, num2);
-  return powerOfTheBaseToTheExponent;
+// const power = (num1, num2) => {
+//   // Used one instead of zero because any number multiple with one is the number itself
+//   let powerOfTheBaseToTheExponent = 1;
+//   const recursiveFunc = (base, exponent) => {
+//     // Base Case (When to stop)
+//     if (exponent === 0) {
+//       return;
+//     }
+//     // Smaller input (To have impact)
+//     powerOfTheBaseToTheExponent *= base;
+//     exponent--;
+//     recursiveFunc(base, exponent);
+//   };
+//   recursiveFunc(num1, num2);
+//   return powerOfTheBaseToTheExponent;
+// };
+
+// console.log(power(2, 5));
+// // result = 4        exponent = 2
+// // result = 16       exponent = 1
+// // result = 64       exponent = 0
+
+// const factorial = (num) => {
+//   if (num === 1) return 1;
+//   return num * factorial(num - 1);
+// };
+
+const factorial = (num) => {
+  let valueToDecrease = num;
+  let result = 1;
+  for (let i = 0; i < num; i++) {
+    result *= valueToDecrease;
+    console.log(num);
+    valueToDecrease--;
+  }
+  return result;
 };
 
-console.log(power(2, 5));
-// result = 4        exponent = 2
-// result = 16       exponent = 1
-// result = 64       exponent = 0
+console.log(factorial(5));
