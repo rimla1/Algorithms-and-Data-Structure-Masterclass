5e3ffe0325170d0c2f0a0cfdf225af33eed15101
Almir Muminovic <projectalmir@gmail.com>
2022-11-26 13:39:20 +0100

    Solving palindore algorithm using recursive function

--- NameOfChapters/recursion/hardChallenges.js
+++ NameOfChapters/recursion/hardChallenges.js
@@ -1,14 +1,26 @@
-const reverse = (string) => {
-  let reverseString = "";
-  let staticValue = string.length;
-  const recursiveFunc = (string) => {
-    if (reverseString.length === staticValue) return;
-    reverseString = reverseString.concat(string[string.length - 1]);
-    let smallerString = string.slice(0, string.length - 1);
-    recursiveFunc(smallerString);
-  };
-  recursiveFunc(string);
-  return reverseString;
+// const reverse = (string) => {
+//   let reverseString = "";
+//   let staticValue = string.length;
+//   const recursiveFunc = (string) => {
+//     if (reverseString.length === staticValue) return;
+//     reverseString = reverseString.concat(string[string.length - 1]);
+//     let smallerString = string.slice(0, string.length - 1);
+//     recursiveFunc(smallerString);
+//   };
+//   recursiveFunc(string);
+//   return reverseString;
+// };
+
+// console.log(reverse("awesome"));
+
+const isPalindrome = (string) => {
+  if (string.length === 0) return;
+  if (string[0] !== string[string.length - 1]) {
+    return false;
+  }
+  let smallerString = string.slice(1, string.length - 1);
+  isPalindrome(smallerString);
+  return true;
 };
 
-console.log(reverse("awesome"));
+console.log(isPalindrome("anna"));
