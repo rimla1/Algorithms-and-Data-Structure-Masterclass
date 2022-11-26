87d1406d4eb51ae1006dd19719c05ec097217edb
Almir Muminovic <projectalmir@gmail.com>
2022-11-26 14:20:05 +0100

    Solving someRecursive using recursive function

--- NameOfChapters/recursion/hardChallenges.js
+++ NameOfChapters/recursion/hardChallenges.js
@@ -13,14 +13,34 @@
 
 // console.log(reverse("awesome"));
 
-const isPalindrome = (string) => {
-  if (string.length === 0) return;
-  if (string[0] !== string[string.length - 1]) {
-    return false;
+// const isPalindrome = (string) => {
+//   if (string.length === 0) return;
+//   if (string[0] !== string[string.length - 1]) {
+//     return false;
+//   }
+//   let smallerString = string.slice(1, string.length - 1);
+//   isPalindrome(smallerString);
+//   return true;
+// };
+
+// console.log(isPalindrome("anna"));
+
+// isOdd
+const someRecursive = (arr) => {
+  if (arr.length === 0) {
+    return;
   }
-  let smallerString = string.slice(1, string.length - 1);
-  isPalindrome(smallerString);
-  return true;
+  if (arr[arr.length - 1] % 2 !== 0) {
+    return true;
+  }
+  arr.pop(arr.length - 1);
+  someRecursive(arr);
+  return false;
 };
 
-console.log(isPalindrome("anna"));
+console.log(someRecursive([2, 4, 6, 8]));
+// [2, 4, 6, 8] -> 4
+// [2, 4, 6]  -> 3
+// [2, 4]   -> 2
+// [2]   -> 1
+// []  -> 0
