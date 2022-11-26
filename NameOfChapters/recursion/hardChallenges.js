5f1e7730e16bb33b07d09d1f15c04a1f09bd114a
Almir Muminovic <projectalmir@gmail.com>
2022-11-26 22:38:08 +0100

    using recursion to upper all letters of each element in an array

--- NameOfChapters/recursion/hardChallenges.js
+++ NameOfChapters/recursion/hardChallenges.js
@@ -26,21 +26,46 @@
 // console.log(isPalindrome("anna"));
 
 // isOdd
-const someRecursive = (arr) => {
-  if (arr.length === 0) {
-    return;
-  }
-  if (arr[arr.length - 1] % 2 !== 0) {
-    return true;
-  }
-  arr.pop(arr.length - 1);
-  someRecursive(arr);
-  return false;
-};
+// const someRecursive = (arr) => {
+//   if (arr.length === 0) {
+//     return;
+//   }
+//   if (arr[arr.length - 1] % 2 !== 0) {
+//     return true;
+//   }
+//   arr.pop(arr.length - 1);
+//   someRecursive(arr);
+//   return false;
+// };
 
-console.log(someRecursive([2, 4, 6, 8]));
+// console.log(someRecursive([2, 4, 6, 8]));
 // [2, 4, 6, 8] -> 4
 // [2, 4, 6]  -> 3
 // [2, 4]   -> 2
 // [2]   -> 1
 // []  -> 0
+
+// function flatten(oldArr){
+//     var newArr = []
+//         for(var i = 0; i < oldArr.length; i++){
+//           if(Array.isArray(oldArr[i])){
+//                 newArr = newArr.concat(flatten(oldArr[i]))
+//           } else {
+//                 newArr.push(oldArr[i])
+//           }
+//     }
+//     return newArr;
+//   }
+
+function capitalizeWords(array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+  let res = capitalizeWords(array.slice(0, -1));
+  res.push(array.slice(array.length - 1)[0].toUpperCase());
+  return res;
+}
+
+console.log(capitalizeWords(["rimla", "ammce", "edo"]));
+
+// ["rimla", "ammce", "edo"] -> arr[0][0]
