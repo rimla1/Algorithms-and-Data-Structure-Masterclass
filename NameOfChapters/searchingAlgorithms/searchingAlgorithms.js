e42fd127c534ce031ba99db9b007cfb69dd727d3
Almir Muminovic <projectalmir@gmail.com>
2022-11-28 14:11:18 +0100

    What indexOd, find and other methods use in background for linear searching

--- NameOfChapters/searchingAlgorithms/searchingAlgorithms.js
+++ NameOfChapters/searchingAlgorithms/searchingAlgorithms.js
@@ -1,6 +1,14 @@
+// Background of indexOf
 const isWantedValueInArray = (array, wantedValue) => {
-  const result = array.indexOf(wantedValue);
-  return result;
+  for (let i = 0; i < array.length; i++) {
+    if (wantedValue === array[i]) return i;
+  }
+  return -1;
 };
 
-console.log(isWantedValueInArray([1, 2, 22, 3, 8, 72, 111, 6], 8));
+// const isWantedValueInArray = (array, wantedValue) => {
+//   const result = array.indexOf(wantedValue);
+//   return result;
+// };
+
+console.log(isWantedValueInArray([1, 2, 22, 3, 8, 72, 111, 6], 23));
