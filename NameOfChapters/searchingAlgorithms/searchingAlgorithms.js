2819e5d5c21cec3c6a88ffcad0f6d673491f2e55
Almir Muminovic <projectalmir@gmail.com>
2022-11-28 20:51:02 +0100

    Refactored version of the solution

--- NameOfChapters/searchingAlgorithms/searchingAlgorithms.js
+++ NameOfChapters/searchingAlgorithms/searchingAlgorithms.js
@@ -31,22 +31,34 @@
 
 // binarySearch([1, 5, 10, 11, 12, 14, 15, 17, 19, 20, 22, 23, 25, 37, 40], 11);
 
+// function binarySearch(arr, elem) {
+//   var start = 0;
+//   var end = arr.length - 1;
+//   var middle = Math.floor((start + end) / 2);
+//   while (arr[middle] !== elem && start <= end) {
+//     if (elem < arr[middle]) {
+//       end = middle - 1;
+//     } else {
+//       start = middle + 1;
+//     }
+//     middle = Math.floor((start + end) / 2);
+//   }
+//   if (arr[middle] === elem) {
+//     return middle;
+//   }
+//   return -1;
+// }
+
 function binarySearch(arr, elem) {
   var start = 0;
   var end = arr.length - 1;
   var middle = Math.floor((start + end) / 2);
   while (arr[middle] !== elem && start <= end) {
-    if (elem < arr[middle]) {
-      end = middle - 1;
-    } else {
-      start = middle + 1;
-    }
+    if (elem < arr[middle]) end = middle - 1;
+    else start = middle + 1;
     middle = Math.floor((start + end) / 2);
   }
-  if (arr[middle] === elem) {
-    return middle;
-  }
-  return -1;
+  return arr[middle] === elem ? middle : -1;
 }
 
 console.log(
