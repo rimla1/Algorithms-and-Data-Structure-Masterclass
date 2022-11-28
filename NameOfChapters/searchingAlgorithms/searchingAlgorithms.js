1219e0ae2db6d24143f55cd9ca7f2051a679120f
Almir Muminovic <projectalmir@gmail.com>
2022-11-28 20:04:37 +0100

    Binary search

--- NameOfChapters/searchingAlgorithms/searchingAlgorithms.js
+++ NameOfChapters/searchingAlgorithms/searchingAlgorithms.js
@@ -12,3 +12,24 @@ const isWantedValueInArray = (array, wantedValue) => {
 // };
 
 console.log(isWantedValueInArray([1, 2, 22, 3, 8, 72, 111, 6], 23));
+
+const binarySearch = (sortedArrayOfPositiveNumbers, wantedNumber) => {
+  let startPointer = 0;
+  let endPointer = sortedArrayOfPositiveNumbers.length - 1;
+  let middlePointer = sortedArrayOfPositiveNumbers.length / 2;
+  let roundedMiddlePointer = Math.round(middlePointer);
+  while (startPointer < endPointer) {
+    if (sortedArrayOfPositiveNumbers[roundedMiddlePointer] < wantedNumber) {
+      startPointer = roundedMiddlePointer + 1;
+    }
+    if (sortedArrayOfPositiveNumbers[roundedMiddlePointer] > wantedNumber) {
+      endPointer = roundedMiddlePointer - 1;
+    }
+    if (sortedArrayOfPositiveNumbers[roundedMiddlePointer] === wantedNumber) {
+      return middlePointer;
+    }
+  }
+  return -1;
+};
+
+binarySearch([1, 5, 10, 11, 12, 14, 15, 17, 19, 20, 22, 23, 25, 37, 40], 11);
