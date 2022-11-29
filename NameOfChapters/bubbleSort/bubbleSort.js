b13ae90ecae442b9b5da392880d3d7954ab73568
Almir Muminovic <projectalmir@gmail.com>
2022-11-30 00:37:20 +0100

    Better version that bubbles largest number at last index of the array

--- NameOfChapters/bubbleSort/bubbleSort.js
+++ NameOfChapters/bubbleSort/bubbleSort.js
@@ -13,22 +13,28 @@
 // [7, 20, 27, 18, 3]
 //     j
 
+// const bubbleSort = (arr) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+//   return arr;
+// };
+
 const bubbleSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
       console.log(arr);
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
+    console.log("J iteracija zavrsena");
   }
   return arr;
 };
 
-//                      i=0
-bubbleSort([23, 1, 5, 22, 74, 40, 3, 5]);
-
-// [1, 23, 5, 22, 74, 40, 3, 5] done
-// [1, 5, 23, 22, 74, 40, 3, 5] done
-// [1, 5, 22, 23, 74, 40, 3, 5] done
-// [1, 5, 22, 23, 74, 40, 3, 5]
+console.log(bubbleSort([23, 1, 5, 22, 74, 40, 3, 5]));
