ee865fd36b0d553e3d5a41370dae83e2f536a3dd
Almir Muminovic <projectalmir@gmail.com>
2022-11-30 00:44:54 +0100

    Using bubbleSort on almost sorted array

--- NameOfChapters/bubbleSort/bubbleSort.js
+++ NameOfChapters/bubbleSort/bubbleSort.js
@@ -24,17 +24,33 @@
 //   return arr;
 // };
 
+// const bubbleSort = (arr) => {
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       console.log(arr);
+//       if (arr[j] > arr[j + 1]) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//     console.log("J iteracija zavrsena");
+//   }
+//   return arr;
+// };
+
 const bubbleSort = (arr) => {
+  let noSwaps;
   for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
       console.log(arr);
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwaps = false;
       }
     }
-    console.log("J iteracija zavrsena");
+    if (noSwaps) break;
   }
   return arr;
 };
 
-console.log(bubbleSort([23, 1, 5, 22, 74, 40, 3, 5]));
+console.log(bubbleSort([6, 1, 2, 3, 4, 5]));
