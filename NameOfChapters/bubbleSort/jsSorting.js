53c171d9ff032dc86a631f0dc7c24b2b983ca38a
Almir Muminovic <projectalmir@gmail.com>
2022-11-29 16:28:51 +0100

    Sorting string with a javascript built-in sort method

---
+++ NameOfChapters/bubbleSort/jsSorting.js
@@ -0,0 +1,9 @@
+const arrayOfString = ["Adam", "Ben", "Daniella", "Cimon", "Eden", "Fabula"];
+const arrayOfNumbers = [1, 8, 3, 100, 26, 2];
+
+const stringSort = (arrayOfString) => {
+  console.log(arrayOfString.sort());
+  return arrayOfString.sort();
+};
+
+stringSort(arrayOfString);
