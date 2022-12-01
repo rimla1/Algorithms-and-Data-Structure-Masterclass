fff641f7497d91c58ae9a87a33889e3011838444
Almir Muminovic <projectalmir@gmail.com>
2022-12-01 16:19:47 +0100

    getting rid of unnecessary checks for switching if the numbers are the lowest

--- NameOfChapters/Sorting/insertionSort.js
+++ NameOfChapters/Sorting/insertionSort.js
@@ -1,17 +1,36 @@
 //   i
 // [35, 100, 50, 2, 20]
 
-const insertionSort = (arr) => {
-  let smallestValue = arr[0];
+// const insertionSort = (arr) => {
+//   let sortedArr = [];
+//   let smallestValue = arr[0];
 
-  for (let i = 1; i < arr.length; i++) {
-    console.log(`Compare currentSmallest: ${smallestValue} with ${arr[i]}`);
-    if (arr[i] < smallestValue) {
-      smallestValue = arr[i];
+//   for (let i = 1; i < arr.length; i++) {
+//     console.log(`Compare currentSmallest: ${smallestValue} with ${arr[i]}`);
+//     if (arr[i] < smallestValue) {
+//       smallestValue = arr[i];
+//     }
+//   }
+//   console.log(smallestValue);
+//   return sortedArr;
+// };
+
+const insertionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let smallest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[smallest]) {
+        smallest = j;
+      }
+    }
+    if (i !== smallest) {
+      console.log(i, smallest);
+      let temp = arr[i];
+      arr[i] = arr[smallest];
+      arr[smallest] = temp;
     }
   }
-  console.log(smallestValue);
   return arr;
 };
 
-console.log(insertionSort([35, 100, 50, 2, 20]));
+console.log(insertionSort([34, 22, 10, 19, 17]));
