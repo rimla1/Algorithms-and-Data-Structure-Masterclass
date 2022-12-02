3dfeed0707838e414b6fceae275535a43cf94e39
Almir Muminovic <projectalmir@gmail.com>
2022-12-02 01:03:28 +0100

    Insertion with scalable code with O(n^2) time complexity

--- NameOfChapters/Sorting/InsertionSort.js
+++ NameOfChapters/Sorting/InsertionSort.js
@@ -1,19 +1,31 @@
+// const insertionSort = (arr) => {
+//   console.log(`Unsorted Array: ${arr}`);
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i - 1] > arr[i]) {
+//       let temp = arr[i - 1];
+//       arr[i - 1] = arr[i];
+//       arr[i] = temp;
+//     }
+//   }
+//   console.log(`Sorted Array: ${arr}`);
+//   return arr;
+// };
+
 const insertionSort = (arr) => {
-  console.log(`Unsorted Array: ${arr}`);
+  let currentVal;
   for (let i = 1; i < arr.length; i++) {
-    if (arr[i - 1] > arr[i]) {
-      let temp = arr[i - 1];
-      arr[i - 1] = arr[i];
-      arr[i] = temp;
+    currentVal = arr[i];
+    console.log(`Current value for iteration${i} is: ${arr[i]}`);
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
     }
+    arr[j + 1] = currentVal;
+    console.log(arr);
   }
-  console.log(`Sorted Array: ${arr}`);
   return arr;
 };
-//                        i-1    i
-console.log(insertionSort([20, 3, 17, 40, 36]));
 
-// [3, 20, 17, 40, 36]
-// [3, 17, 20, 40, 36]
-// [3, 17, 20, 40, 36]
-// [3, 17, 20, 36, 40]
+insertionSort([20, 3, 17, 40, 36, 2]);
+// [3, 20, 17, 40, 36, 2]
+
+// i[1] = 3    j[0] = 20  if(arr[j]: 20 > 3)
