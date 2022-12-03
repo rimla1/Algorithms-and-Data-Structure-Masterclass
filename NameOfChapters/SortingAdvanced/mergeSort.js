0dee4adb89a5b3a8aa18ee7bab9f5d88e7a95fc1
Almir Muminovic <projectalmir@gmail.com>
2022-12-03 15:50:35 +0100

    Merge sorting solution using O(n + m) time complexity

--- NameOfChapters/SortingAdvanced/mergeSort.js
+++ NameOfChapters/SortingAdvanced/mergeSort.js
@@ -1,24 +1,51 @@
+// const merge = (arr1, arr2) => {
+//   let mergedArray = [];
+//   let j = 0;
+//   let k = 0;
+//   for (let i = 0; i < arr2.length * 2; i++) {
+//     let m = i + j;
+//     let n = i + k;
+//     if (arr1[m] < arr2[n]) {
+//       mergedArray.push(arr1[m]);
+//       k--;
+//     }
+//     if (arr1[m] > arr2[n]) {
+//       mergedArray.push(arr2[n]);
+//       j--;
+//     }
+//   }
+
+//   console.log(mergedArray);
+//   return mergedArray;
+// };
+//
+
 const merge = (arr1, arr2) => {
-  let mergedArray = [];
+  let sortedArr = [];
+  let i = 0;
   let j = 0;
-  let k = 0;
-  for (let i = 0; i < arr2.length * 2; i++) {
-    let m = i + j;
-    let n = i + k;
-    if (arr1[m] < arr2[n]) {
-      mergedArray.push(arr1[m]);
-      k--;
-    }
-    if (arr1[m] > arr2[n]) {
-      mergedArray.push(arr2[n]);
-      j--;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      sortedArr.push(arr1[i]);
+      i++;
+    } else {
+      sortedArr.push(arr2[j]);
+      j++;
     }
   }
 
-  console.log(mergedArray);
-  return mergedArray;
+  while (i < arr1.length) {
+    sortedArr.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    sortedArr.push(arr2[j]);
+    j++;
+  }
+  console.log(sortedArr);
+  return sortedArr;
 };
-//
-merge([1, 5, 20, 80], [7, 14, 99, 100, 120]);
+
+merge([1, 5, 20, 1000], [7, 14, 99, 100, 120]);
 
 //
