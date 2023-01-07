cf71ef0293dc927e3479eca494b3f37a9f689259
Almir Muminovic <projectalmir@gmail.com>
2023-01-07 17:14:50 +0100

    Using pivot to keep track of index of locked element

--- NameOfChapters/SortingAdvanced/quickSort.js
+++ NameOfChapters/SortingAdvanced/quickSort.js
@@ -2,13 +2,11 @@ let arr = [22, 4, 5, 10, 17, 200]; // arr.length = 6
 
 const pivot = (arr, startIndex, endIndex) => {
   let pivot = startIndex;
-  console.log(pivot, startIndex, endIndex);
   for (let i = 0; i < endIndex; i++) {
     if (arr[startIndex] > arr[pivot + 1]) {
       pivot++;
     }
   }
-  console.log(pivot);
   return pivot;
 };
 
