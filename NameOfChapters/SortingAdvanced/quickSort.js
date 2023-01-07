67b956011f2899cf8b818f69b30766cb0d6f78a3
Almir Muminovic <projectalmir@gmail.com>
2023-01-07 17:13:57 +0100

    Getting exact index for first item in array to lock it on

--- NameOfChapters/SortingAdvanced/quickSort.js
+++ NameOfChapters/SortingAdvanced/quickSort.js
@@ -1,1 +1,15 @@
-s;
+let arr = [22, 4, 5, 10, 17, 200]; // arr.length = 6
+
+const pivot = (arr, startIndex, endIndex) => {
+  let pivot = startIndex;
+  console.log(pivot, startIndex, endIndex);
+  for (let i = 0; i < endIndex; i++) {
+    if (arr[startIndex] > arr[pivot + 1]) {
+      pivot++;
+    }
+  }
+  console.log(pivot);
+  return pivot;
+};
+
+pivot(arr, 0, arr.length - 1);
