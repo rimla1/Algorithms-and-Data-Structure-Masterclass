a0ae2ae6e2d6161f9ae947b1a07989d78ed16466
Almir Muminovic <projectalmir@gmail.com>
2023-01-07 16:03:09 +0100

    Initial commit for quickSort and finishing mergeSort

--- NameOfChapters/SortingAdvanced/mergeSort.js
+++ NameOfChapters/SortingAdvanced/mergeSort.js
@@ -23,20 +23,18 @@ const merge = (arr1, arr2) => {
     sortedArr.push(arr2[j]);
     j++;
   }
-
   return sortedArr;
 };
 
 // Sort part
 const mergeSort = (arr) => {
-  console.log("Ovo je trenutno array koji proveravamo: ", arr);
   if (arr.length <= 1) {
     return arr;
   }
+
   let mid = Math.floor(arr.length / 2);
   let left = mergeSort(arr.slice(0, mid));
   let right = mergeSort(arr.slice(mid));
-  console.log(left, right);
   return merge(left, right);
 };
 
