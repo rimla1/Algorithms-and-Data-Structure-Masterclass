68f26ad16f16a5b373e2fbbfacac4809618f01e2
Almir Muminovic <projectalmir@gmail.com>
2023-01-08 13:32:22 +0100

    Working with methods when passing an additional parametar to the function

--- NameOfChapters/DataStructure/introduction.js
+++ NameOfChapters/DataStructure/introduction.js
@@ -3,10 +3,12 @@ class Student {
     (this.firstName = firstName),
       (this.secondName = secondName),
       (this.grade = year),
-      (this.absence = 0);
+      (this.absence = 0),
+      (this.scores = []);
   }
   fullName() {
     console.log(`My name is ${this.firstName} ${this.secondName}`);
+    return `My name is ${this.firstName} ${this.secondName}`;
   }
   lateToClass() {
     this.absence++;
@@ -16,12 +18,23 @@ class Student {
       `Absence of student ${this.firstName} ${this.secondName} is: ${this.absence}`
     );
   }
+  addScore(score) {
+    this.scores.push(score);
+    console.log(this.scores);
+    return this.scores;
+  }
+  average() {
+    let sum = this.scores.reduce((a, b) => a + b);
+    let average = sum / this.scores.length;
+    console.log(average);
+    return average;
+  }
 }
 
 const student1 = new Student("Almir", "Muminovic", 4);
 const student2 = new Student("Amel", "Muminovic", 4);
 
-student1.report();
-student1.lateToClass();
-student1.lateToClass();
-student1.report();
+student1.addScore(95);
+student1.addScore(75);
+student1.addScore(100);
+student1.average();
