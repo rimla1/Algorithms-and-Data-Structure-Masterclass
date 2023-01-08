d33dcfbc0b4387c7ef1901ae3ee82d6acf3f1eb5
Almir Muminovic <projectalmir@gmail.com>
2023-01-08 11:54:46 +0100

    Creating instances of a class student with 3 properties

--- NameOfChapters/DataStructure/introduction.js
+++ NameOfChapters/DataStructure/introduction.js
@@ -5,3 +5,9 @@ class Student {
       (this.grade = year);
   }
 }
+
+const student1 = new Student("Almir", "Muminovic", 4);
+const student2 = new Student("Amel", "Muminovic", 4);
+
+console.log(student1.grade);
+console.log(student2.firstName);
