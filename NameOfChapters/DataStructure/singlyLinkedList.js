bc83ee9894a00719849d1c0eaf3885fbac7ceb1e
Almir Muminovic <projectalmir@gmail.com>
2023-01-08 17:32:49 +0100

    professors solution for creating push method

--- NameOfChapters/DataStructure/singlyLinkedList.js
+++ NameOfChapters/DataStructure/singlyLinkedList.js
@@ -12,14 +12,30 @@ class SinglyLinkedList {
     this.length = 0;
   }
 
+  //   push(val) {
+  //     const node = new Node(val);
+  //     if (this.length === 0) {
+  //       this.head = val;
+  //       this.tail = val;
+  //     }
+  //     this.tail = val;
+  //     this.length += 1;
+  //     console.log(this);
+  //     return this;
+  //   }
+
   push(val) {
     const node = new Node(val);
-    if (this.length === 0) {
-      this.head = val;
-      this.tail = val;
+    if (!this.head) {
+      this.head = node;
+      this.tail = this.head;
     }
-    this.tail = val;
+    this.tail.next = node;
+    console.log(this.tail.next);
+    this.tail = node;
     this.length += 1;
+    console.log(this);
+    return this;
   }
 }
 
