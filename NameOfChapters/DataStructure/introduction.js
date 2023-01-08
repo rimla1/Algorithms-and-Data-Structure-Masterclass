73d2338448564ff7f94b18b0fe46d4ddeee3ebeb
Almir Muminovic <projectalmir@gmail.com>
2023-01-08 13:10:26 +0100

    Working with class methods

--- NameOfChapters/DataStructure/introduction.js
+++ NameOfChapters/DataStructure/introduction.js
@@ -2,12 +2,26 @@ class Student {
   constructor(firstName, secondName, year) {
     (this.firstName = firstName),
       (this.secondName = secondName),
-      (this.grade = year);
+      (this.grade = year),
+      (this.absence = 0);
+  }
+  fullName() {
+    console.log(`My name is ${this.firstName} ${this.secondName}`);
+  }
+  lateToClass() {
+    this.absence++;
+  }
+  report() {
+    console.log(
+      `Absence of student ${this.firstName} ${this.secondName} is: ${this.absence}`
+    );
   }
 }
 
 const student1 = new Student("Almir", "Muminovic", 4);
 const student2 = new Student("Amel", "Muminovic", 4);
 
-console.log(student1.grade);
-console.log(student2.firstName);
+student1.report();
+student1.lateToClass();
+student1.lateToClass();
+student1.report();
