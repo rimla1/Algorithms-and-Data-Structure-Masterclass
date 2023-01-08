43231b9a118ddc233e7b06f6a5e325844eef28d7
Almir Muminovic <projectalmir@gmail.com>
2023-01-08 10:15:33 +0100

    Creating radixSort function and using helper function to determe number with largest digit

--- NameOfChapters/SortingAdvanced/radixSort.js
+++ NameOfChapters/SortingAdvanced/radixSort.js
@@ -7,4 +7,13 @@ const digitCount = (num) => {
   return Math.floor(Math.log10(Math.abs(num))) + 1;
 };
 
-console.log(digitCount(4298));
+const radixSort = (arr) => {
+  let largestDigit = 0;
+  for (let i = 0; i < arr.length; i++) {
+    largestDigit = Math.max(digitCount(arr[i]), largestDigit);
+  }
+  console.log(largestDigit);
+  return largestDigit;
+};
+
+radixSort([7, 4, 200, 22, 87, 1594, 9943]);
