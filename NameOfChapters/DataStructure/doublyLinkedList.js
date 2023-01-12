8eba47aad55d03132ab3c594acd6da9192cb5620
Almir Muminovic <projectalmir@gmail.com>
2023-01-12 13:41:52 +0100

    Creating edge case for not-finished push method for doubly linked list

--- NameOfChapters/DataStructure/doublyLinkedList.js
+++ NameOfChapters/DataStructure/doublyLinkedList.js
@@ -12,4 +12,17 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    let node = new Node(val);
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    }
+  }
 }
+
+const list = new DoublyLinkedList();
+list.push(27);
+list.push(21);
+console.log(list);
