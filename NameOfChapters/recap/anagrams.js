49f87c213c6db4eea7e01af0bfaacb0add0f1e02
Almir Muminovic <projectalmir@gmail.com>
2023-10-28 00:38:47 +0200

    recap

---
+++ NameOfChapters/recap/anagrams.js
@@ -0,0 +1,34 @@
+const isAnagram = (string1, string2) => {
+
+    if(string1.length !== string2.length){
+        return false
+    }
+
+    letterCounter1 = {}
+    letterCounter2 = {}
+
+    for (const letter of string1) {
+        letterCounter1[letter] = (letterCounter1[letter] || 0) + 1
+    }
+
+    for (const letter of string2) {
+        letterCounter2[letter] = (letterCounter2[letter] || 0) + 1
+    }
+
+    for (const key in letterCounter1) {
+
+        if(!letterCounter2[key]){
+            return false
+        }
+
+        if(letterCounter1[key] !== letterCounter2[key]){
+            return false
+        }
+    }
+
+    return true
+
+}
+
+
+console.log(isAnagram("almin", "rimla"))
--- NameOfChapters/recap/same.js
+++ NameOfChapters/recap/same.js
@@ -1,16 +1,46 @@
-const same = (arrOfNumbers, arrOfNumbersSquared) => {
-    if(arrOfNumbers.length !== arrOfNumbersSquared.length){
+// const same = (arrOfNumbers, arrOfNumbersSquared) => {
+//     if(arrOfNumbers.length !== arrOfNumbersSquared.length){
+//         return false
+//     }
+//     for(let i = 0; i < arrOfNumbers.length; i++){
+//         let correctIndex = arrOfNumbersSquared.indexOf(arrOfNumbers[i] ** 2)
+//         if(correctIndex === -1){
+//             return false
+//         }
+//         arrOfNumbersSquared.splice(correctIndex, 1)
+//     }
+//     return true
+// }
+
+const same = (array1, array2) => {
+    if(array1.length !== array2.length){
         return false
     }
-    for(let i = 0; i < arrOfNumbers.length; i++){
-        let correctIndex = arrOfNumbersSquared.indexOf(arrOfNumbers[i] ** 2)
-        if(correctIndex === -1){
+
+    const frequencyCounterOfArray1 = {}
+    const frequencyCounterOfArray2 = {}
+
+    for (const num1 of array1) {
+        frequencyCounterOfArray1[num1] = (frequencyCounterOfArray1[num1] || 0) + 1
+    }
+
+    for (const num2 of array2) {
+        frequencyCounterOfArray2[num2] = (frequencyCounterOfArray2[num2] || 0) + 1
+    }
+
+    console.log(frequencyCounterOfArray1)
+    console.log(frequencyCounterOfArray2)
+    for (const key in frequencyCounterOfArray1) {
+        if(!(key ** 2 in frequencyCounterOfArray2)){
             return false
         }
-        arrOfNumbersSquared.splice(correctIndex, 1)
+        if(frequencyCounterOfArray2[key ** 2] !== frequencyCounterOfArray1[key]){
+            return false
+        }
     }
     return true
 }
+
 
 console.log(same([3, 3, 1, 5], [9, 1, 9, 25]))
 // [3, 3, 1, 2]   ===> [9, 1, 4, 9]
