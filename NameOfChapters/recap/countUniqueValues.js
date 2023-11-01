b8143c79bc88e0cc6b196c8c768ce701775ad751
Almir Muminovic <projectalmir@gmail.com>
2023-11-01 13:17:22 +0100

    recap

---
+++ NameOfChapters/recap/countUniqueValues.js
@@ -0,0 +1,14 @@
+const countUniqueValues = (sortedArray) => {
+    let count = {}
+
+    for(let i = 0; i < sortedArray.length; i++){
+        let number = sortedArray[i]
+        count[number] = (count[number] || 0) + 1
+    }
+
+    let arr = Object.keys(count).length
+
+    return arr
+}
+
+countUniqueValues([1, 1, 1, 2, 3, 4, 4, 4, 5, 7])
