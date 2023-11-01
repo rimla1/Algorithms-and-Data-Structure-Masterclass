d295d667f3b0309c387f2953053e76fda314a87b
Almir Muminovic <projectalmir@gmail.com>
2023-11-01 12:32:50 +0100

    Recap

--- NameOfChapters/recap/anagrams.js
+++ NameOfChapters/recap/anagrams.js
@@ -11,12 +11,13 @@ const isAnagram = (string1, string2) => {
         letterCounter1[letter] = (letterCounter1[letter] || 0) + 1
     }
 
+
     for (const letter of string2) {
         letterCounter2[letter] = (letterCounter2[letter] || 0) + 1
     }
 
     for (const key in letterCounter1) {
-
+        
         if(!letterCounter2[key]){
             return false
         }
