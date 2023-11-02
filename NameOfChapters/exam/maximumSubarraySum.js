6617ce0b5888fcc463a7ab5aa3175f3826124bd3
Almir Muminovic <projectalmir@gmail.com>
2023-11-02 18:48:51 +0100

    max sum of a contiguous subarray

--- NameOfChapters/exam/maximumSubarraySum.js
+++ NameOfChapters/exam/maximumSubarraySum.js
@@ -1,3 +1,23 @@
-const maxSubarraySum = () => {
-    
+const maxSubarraySum = (arrayOfNumbers, subarrayLength) => {
+
+    if(subarrayLength > arrayOfNumbers.length){
+        return false
+    }
+
+    let temp = 0
+    let maxSum = 0
+    for(let i = 0; i < subarrayLength; i++){
+        maxSum += arrayOfNumbers[i]
+    }
+    temp = maxSum
+    for(let j = subarrayLength; j < arrayOfNumbers.length; j++){
+        temp = temp - arrayOfNumbers[j - subarrayLength] + arrayOfNumbers[j]
+        maxSum = Math.max(maxSum, temp)
+    }
+
+    return maxSum
+
 }
+
+
+maxSubarraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4], 4)
