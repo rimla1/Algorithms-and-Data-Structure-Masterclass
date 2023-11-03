365d9ccb2d0f68eb992d931dad7a9263e1f49571
Almir Muminovic <projectalmir@gmail.com>
2023-11-03 17:02:44 +0100

    checking if the one string is subsequence of the other string

---
+++ NameOfChapters/exam/isSubsequence.js
@@ -0,0 +1,21 @@
+const isSubsequence = (str1, str2) => {
+    let i = 0;
+    let j = 0;
+    if (!str1) {
+        return true;
+    }
+    while (j < str2.length) {
+      if (str2[j] === str1[i]) {
+        i++;
+      }
+      if (i === str1.length){
+        return true;
+      }
+      j++;
+    }
+    return false;
+  
+}
+          
+console.log(isSubsequence("almir", "walk home almir please"))
+
