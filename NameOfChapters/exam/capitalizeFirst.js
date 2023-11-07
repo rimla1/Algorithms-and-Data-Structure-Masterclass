f7b0eac2472271cbd04346d8e741ed4f7da6ed3f
Almir Muminovic <projectalmir@gmail.com>
2023-11-07 15:40:08 +0100

    Capitalize whole word using recursion

--- NameOfChapters/exam/capitalizeFirst.js
+++ NameOfChapters/exam/capitalizeFirst.js
@@ -15,5 +15,17 @@ const outer = (arrStringsWithLowerCapitalLetter) => {
 
 }
 
-outer(["almir", "john", "adam"])
+// outer(["almir", "john", "adam"])
+
+const capitalizeWords = (array) => {
+    if (array.length === 1) {
+      return [array[0].toUpperCase()];
+    }
+    let res = capitalizeWords(array.slice(0, -1));
+    res.push(array.slice(array.length-1)[0].toUpperCase());
+    return res;
+   
+  }
+
+capitalizeWords(["almir", "john", "adam", "nicole"])
 
