01964fa802a016bba7eb4c926c6e7b829ac784a9
Almir Muminovic <projectalmir@gmail.com>
2023-11-07 12:49:16 +0100

    Using recursion to create product of given array

---
+++ NameOfChapters/exam/productOfArray.js
@@ -0,0 +1,7 @@
+const productOfArray = (arrOfNums) => {
+
+    if(arrOfNums.length === 0) return 0;
+    return arrOfNums[0] + productOfArray(arrOfNums.splice(1))
+}
+
+productOfArray([1,5,20,150,300]) 
