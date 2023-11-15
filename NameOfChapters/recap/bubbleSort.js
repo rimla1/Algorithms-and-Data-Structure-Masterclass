69fd8814558b90f7c5893d70c0c275b217e719b2
Almir Muminovic <projectalmir@gmail.com>
2023-11-15 15:06:11 +0100

    recap

---
+++ NameOfChapters/recap/bubbleSort.js
@@ -0,0 +1,28 @@
+const swap = (arr, indx1, indx2) => {
+    let temp = arr[indx1]
+    arr[indx1] = arr[indx2]
+    arr[indx2] = temp
+}
+
+const bubbleSort = (arr) => {
+    let noSwaps;
+    for(let i = arr.length; i > 0; i--){
+        noSwaps = true
+        for(let j = 0; j < i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                swap(arr, j, j + 1)
+                noSwaps = false
+            }
+        }
+        if(noSwaps){
+            break
+        }
+    }
+    
+    return arr
+}
+
+
+bubbleSort([4,7,12,3,9,33,14])
+
+
