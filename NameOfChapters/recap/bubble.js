497be562061e95d6ccfcac98037db34c07e56e68
Almir Muminovic <projectalmir@gmail.com>
2023-11-15 14:00:40 +0100

    recap

--- NameOfChapters/recap/bubble.js
+++ NameOfChapters/recap/bubble.js
@@ -1,9 +1,13 @@
 const swap = (arr, indx1, indx2) => {
-    console.log('This is how array looks now: ', arr)
     let temp = arr[indx1]
     arr[indx1] = arr[indx2]
     arr[indx2] = temp
-    console.log("This is how array looks after: ", arr)
 }
 
 swap([6,2,3,4,5,1], 0, 5)
+
+const modernSwap = (arr, indx1, indx2) => {
+    [arr[indx1], arr[indx2]] = [arr[indx2], arr[indx1]]
+}
+
+modernSwap([8,2,3,4,5,6,7,1], 0, 7)
