4203adc717cb265983d4fb24dfc0a5266f6113e4
Almir Muminovic <projectalmir@gmail.com>
2023-11-28 17:52:17 +0100

    recap

--- NameOfChapters/recap/class.js
+++ NameOfChapters/recap/class.js
@@ -7,12 +7,15 @@ class Student {
     }
 
     greeting(){
-        console.log(`Hello, ${this.name} ${this.surname}, welcome to year ${this.grade}`)
+        return `Hello, ${this.name} ${this.surname}, welcome to year ${this.grade}`
     }
 
     markAbsence(){
         this.absence += 1
-        console.log(`${this.name} ${this.surname} has been absence: ${this.absence}`)
+        if(this.absence > 3){
+            return 'You have been removed from school!'
+        }
+        return `${this.name} ${this.surname} has been absence: ${this.absence}`
     }
 
 }
@@ -21,5 +24,7 @@ const firstStudent = new Student("Almir", "Muminovic", 4)
 const secondStudent = new Student("Rimla", "Muminovic", 3)
 firstStudent.greeting()
 secondStudent.greeting()
+firstStudent.markAbsence()
+firstStudent.markAbsence()
 firstStudent.markAbsence()
 firstStudent.markAbsence()
