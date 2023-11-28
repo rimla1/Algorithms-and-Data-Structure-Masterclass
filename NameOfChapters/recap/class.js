15f717425b00994f8d01536270ff2cfeb4ed931d
Almir Muminovic <projectalmir@gmail.com>
2023-11-28 17:49:52 +0100

    recap

--- NameOfChapters/recap/class.js
+++ NameOfChapters/recap/class.js
@@ -3,9 +3,23 @@ class Student {
         this.name = name
         this.surname =surname
         this.grade = year
+        this.absence = 0
     }
+
+    greeting(){
+        console.log(`Hello, ${this.name} ${this.surname}, welcome to year ${this.grade}`)
+    }
+
+    markAbsence(){
+        this.absence += 1
+        console.log(`${this.name} ${this.surname} has been absence: ${this.absence}`)
+    }
+
 }
 
-const firstStudent = new Student("Almir", "Muminovic")
+const firstStudent = new Student("Almir", "Muminovic", 4)
 const secondStudent = new Student("Rimla", "Muminovic", 3)
-console.log(firstStudent.name, secondStudent.surname, secondStudent.grade)
+firstStudent.greeting()
+secondStudent.greeting()
+firstStudent.markAbsence()
+firstStudent.markAbsence()
