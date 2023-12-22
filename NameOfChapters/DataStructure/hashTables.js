d41dee04da1f035c121d609b2cf700dd55fac898
Almir Muminovic <projectalmir@gmail.com>
2023-12-22 22:47:09 +0100

    Creating better hash function

--- NameOfChapters/DataStructure/hashTables.js
+++ NameOfChapters/DataStructure/hashTables.js
@@ -8,7 +8,7 @@ const hash = (string, lengthOfArray) => {
     return key
 }
 
-const hashh = (key, arrayLength) => {
+const hashOriginal = (key, arrayLength) => {
     let total = 0
     for (let char of key) {
         let value = char.charCodeAt(0) - 96
@@ -17,7 +17,18 @@ const hashh = (key, arrayLength) => {
     return total
 }
 
-hash("pink", 11)
+const hashOriginalImproved = (key, arrayLength) => {
+    let total = 0
+    let WEIRD_PRIME = 31
+    for(let i = 0; i < Math.min(key.length, 100); i++){
+        let char = key[i]
+        let value = char.charCodeAt(0) - 96
+        total = (total * WEIRD_PRIME + value) % arrayLength
+    }
 
-hashh("pink", 11)
+    return total
+}
+
+hashOriginalImproved("pink", 13)
+hashOriginalImproved("cyan", 13)
 
