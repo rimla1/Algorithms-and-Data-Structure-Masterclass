47ef3dc149bae6ce5dd7bc86be2474a8e6a62012
Almir Muminovic <projectalmir@gmail.com>
2024-01-03 19:55:27 +0100

    longest commong prefix using O(n * m) time complexity

---
+++ NameOfChapters/leetcode/easy/longestCommonPrefix.js
@@ -0,0 +1,26 @@
+const longestCommonPrefix = (strs) => {
+
+    if (strs.length === 1) {
+        return strs[0];
+    }
+
+
+    let prefix = strs[0];
+
+
+    for (let i = 1; i < strs.length; i++) {
+        let j = 0;
+        while (j < prefix.length && j < strs[i].length && prefix[j] === strs[i][j]) {
+            j++;
+        }
+
+
+        prefix = prefix.substring(0, j);
+
+        if (prefix === "") {
+            break;
+        }
+    }
+
+    return prefix;
+};
