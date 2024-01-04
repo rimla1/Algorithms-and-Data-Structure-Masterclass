21d1cccd3a818c52601663428edcc570d3d99dc2
Almir Muminovic <projectalmir@gmail.com>
2024-01-04 11:45:18 +0100

    Solving merge alternately two words without built in methods

--- NameOfChapters/leetcode/easy/mergeAlternately.js
+++ NameOfChapters/leetcode/easy/mergeAlternately.js
@@ -1,12 +1,43 @@
+// const mergeAlternately = (word1, word2) => {
+//     let indexWord1 = 0
+//     let indexWord2 = 0
+//     let result = []
+
+//     while(indexWord1 < word1.length && indexWord2 < word2.length){
+
+//         result.push(word1[indexWord1])
+//         result.push(word2[indexWord2])
+
+//         indexWord1++
+//         indexWord2++
+
+//     }
+
+//     while(indexWord1 < word1.length){
+//         result.push(word1[indexWord1])
+//         indexWord1++
+//     }
+
+//     while(indexWord2 < word2.length){
+//         result.push(word2[indexWord2])
+//         indexWord2++
+//     }
+
+//     const word = result.join("")
+//     return word
+
+    
+// };
+
 const mergeAlternately = (word1, word2) => {
     let indexWord1 = 0
     let indexWord2 = 0
-    let result = []
+    let result = ""
 
     while(indexWord1 < word1.length && indexWord2 < word2.length){
 
-        result.push(word1[indexWord1])
-        result.push(word2[indexWord2])
+        result += word1[indexWord1]
+        result += word2[indexWord2]
 
         indexWord1++
         indexWord2++
@@ -14,17 +45,19 @@ const mergeAlternately = (word1, word2) => {
     }
 
     while(indexWord1 < word1.length){
-        result.push(word1[indexWord1])
+        result += word1[indexWord1]
         indexWord1++
     }
 
     while(indexWord2 < word2.length){
-        result.push(word2[indexWord2])
+        result += word2[indexWord2]
         indexWord2++
     }
 
-    const word = result.join("")
-    return word
+
+    return result
 
     
 };
+
+mergeAlternately("ab", "pqrs")
