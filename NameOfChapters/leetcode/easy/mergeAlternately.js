822c968a71c5a7dfdbd24048e69e9c7a0969d7da
Almir Muminovic <projectalmir@gmail.com>
2024-01-04 11:38:10 +0100

    Merge two words letter by letter

---
+++ NameOfChapters/leetcode/easy/mergeAlternately.js
@@ -0,0 +1,30 @@
+const mergeAlternately = (word1, word2) => {
+    let indexWord1 = 0
+    let indexWord2 = 0
+    let result = []
+
+    while(indexWord1 < word1.length && indexWord2 < word2.length){
+
+        result.push(word1[indexWord1])
+        result.push(word2[indexWord2])
+
+        indexWord1++
+        indexWord2++
+
+    }
+
+    while(indexWord1 < word1.length){
+        result.push(word1[indexWord1])
+        indexWord1++
+    }
+
+    while(indexWord2 < word2.length){
+        result.push(word2[indexWord2])
+        indexWord2++
+    }
+
+    const word = result.join("")
+    return word
+
+    
+};
