1c4e25f2c503c4f422448a701a3d188fe539b10f
Almir Muminovic <projectalmir@gmail.com>
2024-01-10 21:41:54 +0100

    lemonade change problem solved

--- NameOfChapters/leetcode/easy/lemonadeChange.js
+++ NameOfChapters/leetcode/easy/lemonadeChange.js
@@ -32,3 +32,33 @@
 // };
 
 
+const lemonadeChange = (bills) => {
+    let five = 0; 
+    let ten = 0;
+
+    for (const bill of bills) {
+        if (bill === 5) {
+            five++;
+        } else if (bill === 10) {
+            if (five > 0) {
+                five--;
+                ten++;
+            } else {
+                return false; 
+            }
+        } else if (bill === 20) {
+            if (ten > 0 && five > 0) {
+                ten--;
+                five--;
+            } else if (five >= 3) {
+                five -= 3;
+            } else {
+                return false; 
+            }
+        }
+    }
+
+    return true; 
+}
+
+
