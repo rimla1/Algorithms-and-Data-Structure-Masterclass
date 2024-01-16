d91e5600ac5fddeae3c27bd2cdb5070625abafb2
Almir Muminovic <projectalmir@gmail.com>
2024-01-16 12:40:18 +0100

    contains duplicate

---
+++ NameOfChapters/leetcode/easy/containsDuplicate.js
@@ -0,0 +1,10 @@
+const containsDuplicate = (nums) => {
+    let seen = new Set(); 
+    for (let num of nums) { 
+        if (seen.has(num)) { 
+            return true; 
+            } 
+        seen.add(num); 
+    } 
+    return false; 
+};
