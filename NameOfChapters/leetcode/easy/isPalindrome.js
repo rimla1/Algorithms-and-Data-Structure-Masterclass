7fd08aa2059f7eff91c388d577fe2566517f9b1c
Almir Muminovic <projectalmir@gmail.com>
2024-01-19 19:43:30 +0100

    Trying to solve problem with O(1) space complexity

--- NameOfChapters/leetcode/easy/isPalindrome.js
+++ NameOfChapters/leetcode/easy/isPalindrome.js
@@ -1,25 +1,40 @@
+// const isPalindrome = (s) => {
+//     if(s.length === 0) return true
+//     let validString = ""
+//     for(let i = 0; i < s.length; i++){
+//         if((s[i].charCodeAt() >= 97 && s[i].charCodeAt() <= 122) || (s[i].charCodeAt() >= 48 && s[i].charCodeAt() <= 57)){
+//             validString = validString + s[i]
+//         }
+//         if(s[i].charCodeAt() >= 65 && s[i].charCodeAt() <= 90){
+//             let character = String.fromCharCode(s[i].charCodeAt() + 32)
+//             validString = validString + character
+//         }
+//     }
+//     let left = 0
+//     let right = validString.length - 1
+//     while(left <= right){
+//         if(validString[left] !== validString[right]) return false
+//         left++
+//         right--
+//     }
+//     return true
+// };
+
 const isPalindrome = (s) => {
     if(s.length === 0) return true
-    let validString = ""
-    for(let i = 0; i < s.length; i++){
-        if((s[i].charCodeAt() >= 97 && s[i].charCodeAt() <= 122) || (s[i].charCodeAt() >= 48 && s[i].charCodeAt() <= 57)){
-            validString = validString + s[i]
-        }
-        if(s[i].charCodeAt() >= 65 && s[i].charCodeAt() <= 90){
-            let character = String.fromCharCode(s[i].charCodeAt() + 32)
-            validString = validString + character
-        }
-    }
     let left = 0
-    let right = validString.length - 1
-    while(left <= right){
-        if(validString[left] !== validString[right]) return false
-        left++
-        right--
+    let right = s.length - 1
+    while(left < right){
+        if(s[left] !== s[right] && s[left].charCodeAt() + 32 !== s[left].charCodeAt() && s[left].charCodeAt() !== s[left].charCodeAt() + 32) return false
     }
 
     return true
 
-};
+}
 
+console.log("a".charCodeAt()) // 97
+console.log("A".charCodeAt()) // 65
+console.log("z".charCodeAt()) // 122
 
+// console.log("0".charCodeAt()) // 48
+// console.log("9".charCodeAt()) // 57
