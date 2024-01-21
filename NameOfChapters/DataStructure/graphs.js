963c2521d96e1ed31464cd640c3d665a3a3c5dbb
Almir Muminovic <projectalmir@gmail.com>
2024-01-21 21:37:15 +0100

    Creating add edge method in undirected unweighted graph

--- NameOfChapters/DataStructure/graphs.js
+++ NameOfChapters/DataStructure/graphs.js
@@ -7,6 +7,13 @@ class Graph {
     if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
   }
 
+  addEdge(vertex1, vertex2){
+    if(vertex1 && vertex2){
+        this.adjacencyList[vertex1].push(vertex2)
+        this.adjacencyList[vertex2].push(vertex1)
+    }
+  }
+
   getAdjacencyList(){
     console.log(this.adjacencyList)
   }
@@ -14,8 +21,15 @@ class Graph {
 }
 
 const graph = new Graph()
-graph.addVertex("Eclipse")
-graph.addVertex("Dzungla")
-graph.addVertex("Hrvati")
+graph.addVertex("Tokyo")
+graph.addVertex("Paris")
+graph.addVertex("Berlin")
+graph.addVertex("Belgrade")
+graph.getAdjacencyList()
+graph.addEdge("Belgrade", "Berlin")
+graph.addEdge("Belgrade", "Paris")
+graph.addEdge("Paris", "Berlin")
+graph.addEdge("Tokyo", "Berlin")
+graph.addEdge("Tokyo", "Paris")
 graph.getAdjacencyList()
 
