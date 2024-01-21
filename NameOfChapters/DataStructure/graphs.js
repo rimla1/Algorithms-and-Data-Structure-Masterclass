43a066169c83d04861d2f808bfcfd717d8e1dc4a
Almir Muminovic <projectalmir@gmail.com>
2024-01-21 21:22:49 +0100

    Simple check to prevent duplicates

--- NameOfChapters/DataStructure/graphs.js
+++ NameOfChapters/DataStructure/graphs.js
@@ -3,8 +3,8 @@ class Graph {
     this.adjacencyList = {};
   }
 
-  addVertex(name){
-    this.adjacencyList[name] = []
+  addVertex(vertex){
+    if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
   }
 
   getAdjacencyList(){
