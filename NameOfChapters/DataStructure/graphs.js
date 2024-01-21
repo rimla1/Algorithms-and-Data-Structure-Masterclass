526baba53d5fcd5594f487aec16b652970dd57ae
Almir Muminovic <projectalmir@gmail.com>
2024-01-21 21:20:03 +0100

    Creating add vertex method

--- NameOfChapters/DataStructure/graphs.js
+++ NameOfChapters/DataStructure/graphs.js
@@ -2,4 +2,20 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
+  addVertex(name){
+    this.adjacencyList[name] = []
+  }
+
+  getAdjacencyList(){
+    console.log(this.adjacencyList)
+  }
+
 }
+
+const graph = new Graph()
+graph.addVertex("Eclipse")
+graph.addVertex("Dzungla")
+graph.addVertex("Hrvati")
+graph.getAdjacencyList()
+
