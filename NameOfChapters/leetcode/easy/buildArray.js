364c45421f628aba5658a1a8c3d245f3ed6f33b7
Almir Muminovic <projectalmir@gmail.com>
2024-01-24 13:22:11 +0100

    Build array

---
+++ NameOfChapters/leetcode/easy/buildArray.js
@@ -0,0 +1,13 @@
+const buildArray = (nums) => {
+    const n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        nums[i] += n * (nums[nums[i]] % n);
+    }
+
+    for (let i = 0; i < n; i++) {
+        nums[i] = Math.floor(nums[i] / n);
+    }
+
+    return nums;
+}
