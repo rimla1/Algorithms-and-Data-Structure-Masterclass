9f6f3b458b1830c5000dffb2853ff18e79aa6611
Almir Muminovic <projectalmir@gmail.com>
2024-01-24 13:49:09 +0100

    Solving leetcode build array problem

--- NameOfChapters/leetcode/easy/buildArray.js
+++ NameOfChapters/leetcode/easy/buildArray.js
@@ -2,7 +2,9 @@ const buildArray = (nums) => {
     const n = nums.length;
 
     for (let i = 0; i < n; i++) {
+        console.log(nums[i])
         nums[i] += n * (nums[nums[i]] % n);
+        console.log(nums[i])
     }
 
     for (let i = 0; i < n; i++) {
@@ -11,3 +13,5 @@ const buildArray = (nums) => {
 
     return nums;
 }
+
+buildArray([0,2,1,5,3,4])
