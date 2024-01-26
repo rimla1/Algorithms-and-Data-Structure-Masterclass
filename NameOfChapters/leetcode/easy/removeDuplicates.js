a76c16be8918f103a6cd65eca507180877c63b5d
Almir Muminovic <projectalmir@gmail.com>
2024-01-26 15:07:41 +0100

    cleaner solution with only if statement

--- NameOfChapters/leetcode/easy/removeDuplicates.js
+++ NameOfChapters/leetcode/easy/removeDuplicates.js
@@ -1,8 +1,7 @@
 const removeDuplicates = (nums) => {
     let k = 1
     for(let i = 1; i < nums.length; i++){
-        if(nums[i] === nums[i - 1]) continue
-        else {
+        if(nums[i] !== nums[i - 1]){
             nums[k] = nums[i]
             k++
         }
