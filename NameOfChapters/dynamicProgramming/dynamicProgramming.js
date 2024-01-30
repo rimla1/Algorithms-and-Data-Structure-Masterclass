5a036f1e0401a9d48dce825eb7240efb32ec0a94
Almir Muminovic <projectalmir@gmail.com>
2024-01-30 18:45:54 +0100

    dynamic programming

--- NameOfChapters/dynamicProgramming/dynamicProgramming.js
+++ NameOfChapters/dynamicProgramming/dynamicProgramming.js
@@ -3,3 +3,13 @@ const fib = (n) => {
     if(n <= 2) return 1
     return fib(n - 1) + fib(n - 2)
 }
+
+
+const fibMemo = (n, memo=[]) => {
+    if(memo[n] !== undefined) return memo[n]
+    if(n <= 2) return 1
+    let res = fibMemo(n - 1, memo) + fibMemo(n - 2, memo)
+    memo[n] = res
+    return res
+}
+
